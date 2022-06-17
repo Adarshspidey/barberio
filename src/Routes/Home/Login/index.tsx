b89cdeaa6e5b13bf5fb8bb9578e76bf9b2a8b740
Adarsh A J <68109191+Adarshspidey@users.noreply.github.com>
2022-06-17 10:44:07 +0530

    Login Validation

--- src/Routes/Home/Login/index.tsx
+++ src/Routes/Home/Login/index.tsx
@@ -1,17 +1,83 @@
-import { Dispatch, SetStateAction, useEffect } from "react";
+import {
+  Dispatch,
+  SetStateAction,
+  SyntheticEvent,
+  useEffect,
+  useState,
+} from "react";
 import "./style.css";
+import InputField from "../../../Components/Input";
 import loginImage from "../../../assets/Icons/login.svg";
 import { useNavigate } from "react-router-dom";
+import { ShopLogin, ShopLoginError } from "../../../Types/Shop";
+import postCall from "../../../Services/postCall";
+import { ValidationError } from "../../../Types/Error";
+import useIdleCall from "../../../Hooks/useIdleCall";
 
 interface PropsTypes {
   setHeaderImage: Dispatch<SetStateAction<string>>;
 }
 
+const emptyForm: ShopLogin = {
+  phone: "",
+};
+
 const Login = ({ setHeaderImage }: PropsTypes) => {
+  const [loginData, setLoginData] = useState<ShopLogin>({ ...emptyForm });
+
+  const { phone } = loginData;
+
+  const [loginErrorData, setLoginErrorData] = useState<ShopLoginError>({
+    ...emptyForm,
+  });
+
+  const [currentUpdatingField, setCurrentUpdatingField] = useState<string>("");
+
+  const [submitted, setSubmitted] = useState<boolean>(false);
+
+  useIdleCall(
+    () => {
+      currentUpdatingField && validate(currentUpdatingField);
+    },
+    [loginData],
+    1000
+  );
+
+  const onChange = (key: string, value: string) => {
+    setCurrentUpdatingField(key);
+    setLoginData((prev) => ({ ...prev, [key]: value }));
+  };
+
+  const onErrorChange = (key: string, value: string) => {
+    setLoginErrorData((prev) => ({ ...prev, [key]: value }));
+  };
+
+  const validate = async (key: string) => {
+    setLoginErrorData((prev) => ({ ...prev, [key]: "" }));
+    const result = await postCall("/auth/login/validate", loginData);
+    if (!result?.status) {
+      return result.data.forEach(({ path, message }: ValidationError) => {
+        if (key === path) onErrorChange(path, message);
+      });
+    }
+  };
+
   const navigate = useNavigate();
   useEffect(() => {
     setHeaderImage(loginImage);
   });
+
+  const submit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+    setSubmitted(true);
+    const result = await postCall("/auth/login", loginData);
+    if (!result?.status) {
+      return result.data.forEach(({ path, message }: ValidationError) => {
+        onErrorChange(path, message);
+      });
+    }
+    return navigate("/otp");
+  };
 
   return (
     <div>
@@ -29,14 +95,30 @@ const Login = ({ setHeaderImage }: PropsTypes) => {
       <div>Welcome Back</div>
       <div>Remember me? I'm your barberio.</div>
 
-      <input type="text" />
+      <form onSubmit={submit}>
+        <div className="form-container">
+          <InputField
+            label="Phone Number"
+            value={phone}
+            submitted={submitted}
+            error={loginErrorData.phone}
+            onChange={(value) => onChange("phone", value)}
+          />
+
+          <button className="register-button" type="submit">
+            Login
+          </button>
+        </div>
+      </form>
+
+      {/* <input type="text" />
       <button
         onClick={() => {
           navigate("/otp");
         }}
       >
         Login
-      </button>
+      </button> */}
     </div>
   );
 };
--- src/Types/Shop/index.ts
+++ src/Types/Shop/index.ts
@@ -1,14 +1,21 @@
 export type ShopRegister = {
-    name: string;
-    phone: string;
-    registerNumber: string;
-    ownerName: string;
-}
+  name: string;
+  phone: string;
+  registerNumber: string;
+  ownerName: string;
+};
 
 export type ShopRegisterError = {
-    name: string;
-    phone: string;
-    registerNumber: string;
-    ownerName: string;
-}
+  name: string;
+  phone: string;
+  registerNumber: string;
+  ownerName: string;
+};
 
+export type ShopLogin = {
+  phone: string;
+};
+
+export type ShopLoginError = {
+  phone: string;
+};
