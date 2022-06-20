684f5406d00880e407dd482d511e9d89ba28c0e5
shini20 <shinianitha99@gmail.com>
2022-06-20 15:02:04 +0530

    service card style ok

--- src/Components/ServiceCard/index.tsx
+++ src/Components/ServiceCard/index.tsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import { useNavigate } from "react-router-dom";
+import SmallButton from '../Buttons/SmallButton';
 interface ServicecardProps {
     title: string
   }
 const ServiceCard = ({title}:ServicecardProps) => {
     const navigate = useNavigate();
   return (
-    <div>
-        {title}
-        <button onClick={() => {
+    <div className="booking-card-component">
+      <div className="booking-card-duration-component">
+      <div className="booking-card-title">{title}</div>
+        <SmallButton 
+        type="black"
+        label="Disable"
+        onClick={() => {
               navigate("/shop/disable-service");
-            }}>disable</button>
+            }}/>
+      </div>
     </div>
   )
 }
