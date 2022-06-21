26a701790956e74753f4021fc4dfad410d6a7805
Adarsh A J <68109191+Adarshspidey@users.noreply.github.com>
2022-06-21 14:09:06 +0530

    Upload Icon incomplete

--- src/Components/UploadIcon/index.tsx
+++ src/Components/UploadIcon/index.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { useState } from "react";
 
 const UploadIcon = () => {
-  return <div>UploadIcon</div>;
+  const [selectedFile, setSelectedFile] = useState();
+
+  const changeHandler = (event: any) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  return (
+    <div>
+      <input type="file" name="file" onChange={changeHandler} />
+      <div className="add-upload-icon"></div>
+    </div>
+  );
 };
 
 export default UploadIcon;
