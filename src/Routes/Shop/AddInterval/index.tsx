62d83a6a617bab0157dddea5367f3f8f7d421396
Adarsh A J <68109191+Adarshspidey@users.noreply.github.com>
2022-06-22 17:28:40 +0530

    service

--- src/Routes/Shop/AddInterval/index.tsx
+++ src/Routes/Shop/AddInterval/index.tsx
@@ -1,17 +1,37 @@
 import { useNavigate } from "react-router-dom";
-import InputField from "../../../Components/Input";
-import time from "../../../assets/Icons/time.svg";
-import arrow from "../../../assets/Icons/arrow-down.svg";
 import BigButton from "../../../Components/Buttons/BigButton";
 import IntervalListCard from "../../../Components/IntervalListCard";
 import ButtonWithIcon from "../../../Components/Buttons/ButtonWithIcon";
-import addplus from '../../../assets/Icons/add-plus.svg'
+import addplus from "../../../assets/Icons/add-plus.svg";
 
 const AddInterval = () => {
   const navigate = useNavigate();
   return (
     <div>
       <div className="set-up-question">Intervals</div>
+
+      <div className="button-end-with-margin">
+        <ButtonWithIcon
+          type="white"
+          label="Set By Service"
+          onClick={() => {
+            navigate("/setup/interval-form");
+          }}
+        />
+      </div>
+
+      <div className="sub-content-header">Common Interval</div>
+      <div className="button-end">
+        <ButtonWithIcon
+          rightIcon={addplus}
+          type="white"
+          label="Add"
+          onClick={() => {
+            navigate("/setup/interval-form");
+          }}
+        />
+      </div>
+
       <div>
         <div className="input-wrapper">
           <div
@@ -31,31 +51,6 @@ const AddInterval = () => {
           </div>
         </div>
       </div>
-
-      <div className="sub-content-header">Common Interval</div>
-      <div className="button-end">
-      <ButtonWithIcon
-          rightIcon={addplus}
-          type="white"
-          label="Add"
-          onClick={() => {
-            navigate("/setup/interval-form");
-          }}/>
-      </div>
-      <InputField
-        label="Start Time"
-        onChange={() => {}}
-        value=""
-        icon={time}
-        arrow={arrow}
-      />
-      <InputField
-        label="End Time"
-        onChange={() => {}}
-        value=""
-        icon={time}
-        arrow={arrow}
-      />
       <div className="button-bottom-wrapper">
         <BigButton
           type="primary"
