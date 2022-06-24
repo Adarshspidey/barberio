ba6a027871ed4a012d36b03118808a4aad4226f1
shini20 <shinianitha99@gmail.com>
2022-06-24 16:51:10 +0530

    Upload Image ok

--- src/Routes/Shop/ImagePage/index.tsx
+++ src/Routes/Shop/ImagePage/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ButtonWithIcon from "../../../Components/Buttons/ButtonWithIcon";
 import upload from "../../../assets/Icons/upload-icon-white.svg";
+import UploadImageCard from "../../../Components/UploadImageCard";
 // import delete from "../../../assets/Icons/delete-icon-white.svg";
 
 const ImagePage = () => {
@@ -9,16 +10,16 @@ const ImagePage = () => {
       <div className="wrapper-flex-justify-content">
         <div className="edit-profile-title">Images</div>
       </div>
-      <div className="image-button-wrapper">
-        <div className="image-button-title">Cover Page</div>
-        <ButtonWithIcon label="Upload" type="green" leftIcon={upload} />
+      
+        <UploadImageCard title="Cover Page"/>
+        {/* <div className="image-button-title">Cover Page</div>
+        <ButtonWithIcon label="Upload" type="green" leftIcon={upload} /> */}
 
-        <div className="image-button-title">Logo/Brand Image</div>
+        {/* <div className="image-button-title">Logo/Brand Image</div>
         <ButtonWithIcon label="Upload" type="green" leftIcon={upload} />
 
         <div className="image-button-title">Image Gallery</div>
-        <ButtonWithIcon label="Upload" type="green" leftIcon={upload} />
-      </div>
+        <ButtonWithIcon label="Upload" type="green" leftIcon={upload} /> */}
     </div>
   );
 };
