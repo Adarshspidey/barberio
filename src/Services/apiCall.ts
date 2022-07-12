a3de456fcdbf9751ff4777eebf6b60167d90d953
Afsal Khan <afsalkhanmdt@gmail.com>
2022-07-12 09:25:09 +0530

    aupdate for file upload

--- src/Services/apiCall.ts
+++ src/Services/apiCall.ts
@@ -1,21 +1,26 @@
 import apiUrl from "../Config/apiUrl";
 
 export default async (url: string, method: string, data?:any, isFile = false)=>{
+    const formData = new FormData();
     if(isFile && data instanceof File){
-        const formData = new FormData();
         formData.append("file", data);
     }
-    let response = await fetch(`${apiUrl}${url}`,{
-        method,
-        headers: isFile
-        ? undefined
-        : {
-           "Content-Type" : "application/json",
-              "Authorization" : `Bearer ${localStorage.getItem("token")}`
-         },
-        body: (isFile && FormData) || data ? JSON.stringify(data) : undefined,
+    const headers = new Headers();
 
-    });
+    if(!isFile)
+        headers.append("Content-Type", "application/json");
+
+    if(localStorage.getItem("token"))
+        headers.append("Authorization", `Bearer ${localStorage.getItem("token")}`);
+    
+    let response = await fetch(
+        `${apiUrl}${url}`,
+        {
+        method,
+        headers,
+        body: isFile? formData : data ? JSON.stringify(data) : undefined,
+        }
+    );
     return response.json();
 
 }
