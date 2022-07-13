ee156681a0a729848ea30d53d11102d92214fa6c
shini20 <shinianitha99@gmail.com>
2022-07-13 11:25:07 +0530

    shedule day

--- src/Routes/Shop/ScheduleTime/index.tsx
+++ src/Routes/Shop/ScheduleTime/index.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction, useEffect } from "react";
+import React, { Dispatch, SetStateAction, SyntheticEvent, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import InputField from "../../../Components/Input";
 import time from "../../../assets/Icons/time.svg";
@@ -7,12 +7,68 @@ import BigButton from "../../../Components/Buttons/BigButton";
 import ButtonWithIcon from "../../../Components/Buttons/ButtonWithIcon";
 import { ShopSetupLayOutProps } from "../../../Types/Props";
 import ServiceCalender from "../../../Components/Calendar/ServiceCalender";
+import { ShopSheduleday, ShopSheduledayError } from "../../../Types/Shop";
+import useIdleCall from "../../../Hooks/useIdleCall";
+import postCall from "../../../Services/postCall";
+import { ValidationError } from "../../../Types/Error";
 
 interface PropsType {
   setShopSetupLayoutProps: Dispatch<SetStateAction<ShopSetupLayOutProps>>;
 }
 
 const ScheduleTime = ({ setShopSetupLayoutProps }: PropsType) => {
+
+  const emptyForm: ShopSheduleday = {
+    startTime: 10,
+    endTime:11,
+  };
+
+  const [sheduleData, setsheduleData] = useState<ShopSheduleday>({ ...emptyForm });
+  const { startTime,endTime } = sheduleData;
+  const [ErrorData, setErrorData] = useState<ShopSheduledayError>({
+    ...emptyForm,
+  });
+  const [currentUpdatingField, setCurrentUpdatingField] = useState<string>("");
+
+  useIdleCall(
+    () => {
+      currentUpdatingField && validate(currentUpdatingField);
+    },
+    [sheduleData],
+    1000
+  );
+
+  const onErrorChange = (key: string, value: string) => {
+    setErrorData((prev) => ({ ...prev, [key]: value }));
+  };
+  const validate = async (key: string) => {
+    setErrorData((prev) => ({ ...prev, [key]: "" }));
+    const result = await postCall("/shop/add-location", sheduleData);
+
+    if (!result?.status) {
+      return result.data.forEach(({ path, message }: ValidationError) => {
+        if (key === path) onErrorChange(path, message);
+      });
+    }
+  };
+
+  const onChange = (key: string, value: string) => {
+    setCurrentUpdatingField(key);
+    setsheduleData((prev) => ({ ...prev, [key]: value }));
+  };
+
+  const submit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+    const result = await postCall("/auth/login", sheduleData);
+    if (!result?.status) {
+      console.log(result.data);
+      return result.data.forEach(({ path, message }: ValidationError) => {
+        onErrorChange(path, message);
+      });
+      return;
+    }
+  };
+
   useEffect(() => {
     setShopSetupLayoutProps((prev) => ({
       ...prev,
@@ -39,11 +95,11 @@ const ScheduleTime = ({ setShopSetupLayoutProps }: PropsType) => {
       </div>
 
       <div><ServiceCalender/></div>
-      
+      <form onSubmit={submit}>
       <div className="input-wrapper">
         <InputField
           label="Start Time"
-          onChange={() => {}}
+          onChange={(value) => onChange("startTime", value)}
           value=""
           icon={time}
           arrow={arrow}
@@ -51,13 +107,12 @@ const ScheduleTime = ({ setShopSetupLayoutProps }: PropsType) => {
 
         <InputField
           label="End Time"
-          onChange={() => {}}
+          onChange={(value) => onChange("endTime", value)}
           value=""
           icon={time}
           arrow={arrow}
         />
       </div>
-
       <div className="button-bottom-wrapper">
         <BigButton
           type="primary"
@@ -67,6 +122,7 @@ const ScheduleTime = ({ setShopSetupLayoutProps }: PropsType) => {
           }}
         />
       </div>
+      </form>
     </div>
   );
 };
