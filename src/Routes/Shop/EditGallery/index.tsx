1857802eca285c8940174db8a51f1e2f7d38d8fd
shini20 <shinianitha99@gmail.com>
2022-07-14 16:45:56 +0530

    Galery Upload Button UI

--- src/Routes/Shop/EditGallery/index.tsx
+++ src/Routes/Shop/EditGallery/index.tsx
@@ -1,10 +1,18 @@
 import gallery from "../../../assets/Images/galllerycollection.png"
+import ButtonWithIcon from "../../../Components/Buttons/ButtonWithIcon"
 import ImageGalleryCard from "../../../Components/ImageGalleryCard"
+import upload from '../../../assets/Icons/upload.svg';
 const EditGallery = () => {
   return (
     <div className="waper-main-container">
         <div className="wrapper-flex-justify-content">
-          <div className="edit-profile-title"> Gallery</div>
+          <div className="gallery-group">
+          <div> Gallery</div>
+          <ButtonWithIcon 
+          leftIcon={upload}
+          label="upload"
+          type="upload"/>
+          </div>
         </div>
         <div className="gallery-wapper-container">
           <ImageGalleryCard
